0825bdc9e4eceb6235be8af6f2b98e7667699f72
Peter <plaloisi@gmail.com>
2022-04-05 14:22:09 -0500

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-#updates most recent copy of the checker to the src/rover/lib/bin (which is used in the hardcoded path in checker2/Rover/runRover.sh)
-
+#updates most recent copy of the checker to the src/rover/main/bin (which is used in the hardcoded path in checker2/Rover/runRover.sh)
+sudo -u tomcat cp checker2/Rover/bin /srv/rover/main/bin 
 #then make war file out of website and send it to /opt/tomcat/webapps where it will be automatically deployed by tomcat
 #NOTE, this assumes tomcat is running using the user tomcat, this should not be performed as root
 cd website
