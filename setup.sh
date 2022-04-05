a923bc2a454e666a52fce47c871574f3bef89391
Peter <plaloisi@gmail.com>
2022-04-05 01:26:33 -0500

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 
 #move rover resources from /setup-resources to /srv/rover   (note: website calls process which should should run from and use resources from srv/rover (as srv is proper place for server flies like these))
-sudo cp /setup-resources/rover -r /srv/  #copy the rover resources to srv/rover, but this leaves it owned by root so the next command fixes this
-sudo chown -R tomcat:tomcat srv/rover #set owner to tomcat
+sudo cp setup-resources/rover/ -r /srv/  #copy the rover resources to srv/rover, but this leaves it owned by root so the next command fixes this
+sudo chown -R tomcat:tomcat /srv/rover #set owner to tomcat
 
 #make war file from website and move it to /opt/tomcat/webapps where it will be deployed
 ./deploy.sh
