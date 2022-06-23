7524bea4bec0425854abf96e0480436f2615c708
logan-larson <loganlarson20@gmail.com>
2022-06-23 12:30:52 -0400

    Init spring boot in project

---
+++ src/main/java/edu/hci/SocialVerificationWebsite/SocialVerificationWebsiteApplication.java
@@ -0,0 +1,14 @@
+package edu.hci.SocialVerificationWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SocialVerificationWebsiteApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialVerificationWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/hci/SocialVerificationWebsite/SocialVerificationWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hci.SocialVerificationWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialVerificationWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
